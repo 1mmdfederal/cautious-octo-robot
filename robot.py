c3d8b91f684ccb7193492fa871b323fb004fecf4
1mmdfederal <102861370+1mmdfederal@users.noreply.github.com>
2022-04-04 22:58:49 +0430

    robot.py

---
+++ robot.py
@@ -0,0 +1,37 @@
+# git clone https://github.com/1mmdfederal/cautious-octo-robot.git
+# cd cautious-octo-robot.git
+# insragram * m__17__34
+# rubika + cr_federal
+# pkg install python3 
+# pkg python3 
+# cautious-octo-robot.py
+#.python3 cautious-octo-robot.py
+# ..m
+# ...m
+# ....d
+# .....f
+# ......e
+# .......d
+# ........e
+# .........r
+# ..........a
+# ...........l
+
+# https://github.com/1mmdfederal/cautious-octo-robot.git (n123k)
+.mtj.tmp/
+
+# mmd federal rubika @cr_federal 
+# instagram m__17__34
+
+# Package Files # zip
+# pkg install git
+# pkg install figlet 
+# figlet 
+# apt install lolcat
+# pkg install python3
+# pkg install unzip
+# pkg install python2
+# pkg install python
+
+# virtual machine crash logs, see http://www.java.com/en/download/help/error_hotspot.xml
+hs_err_pid*
