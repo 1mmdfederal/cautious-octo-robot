89eac2fa1c4e9cb844fe2eb2db846e3df06c2670
1mmdfederal <102861370+1mmdfederal@users.noreply.github.com>
2022-04-04 23:17:50 +0430

    Update cautious-octo-robot.py

--- cautious-octo-robot.py
+++ cautious-octo-robot.py
@@ -2,13 +2,11 @@
  cd cautious-octo-robot
  insragram * m__17__34
  rubika + cr_federal
- pkg install python3 
- pkg python3 
  cautious-octo-robot.py
 python3 cautious-octo-robot.py
  ..m
  ...m
-....d
+ ....d
  .....f
  ......e
  .......d
@@ -17,21 +15,22 @@ python3 cautious-octo-robot.py
  ..........a
  ...........l
 
- https://github.com/1mmdfederal/cautious-octo-robot.git (n123k)
-.mtj.tmp/
+ 
+ virtual machine crash logs, see http://www.java.com/en/download/help/error_hotspot.xml
+hs_err_pid*
 
- mmd federal rubika @cr_federal 
- instagram m__17__34
+ "mmd federal" "rubika" "@cr_federal" 
+ "instagram m__17__34"
 
- Package Files  zip
- pkg install git
+ "Package Files"  zip
+ "pkg install git"
  pkg install figlet 
  figlet 
  apt install lolcat
  pkg install python3
  pkg install unzip
  pkg install python2
- pkg install python
+ "pkg install python"
 
  virtual machine crash logs, see http://www.java.com/en/download/help/error_hotspot.xml
 hs_err_pid*
